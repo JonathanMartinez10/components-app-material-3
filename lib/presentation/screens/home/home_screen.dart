06c6cb9ab9aa47c20d11bcc49605606e0345a9cc
Jonathan Martinez <jonathanmartzca@gmail.com>
2023-06-18 14:28:34 -0600

    basic routes configured

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -29,7 +30,7 @@ class _HomeView extends StatelessWidget {
         
         final menuItem = appMenuItems[index];
 
-        return _CustomListTile(item: menuItem);
+        return _CustomListTile(menuItem: menuItem);
 
       },
     );
@@ -38,10 +39,10 @@ class _HomeView extends StatelessWidget {
 
 class _CustomListTile extends StatelessWidget {
   const _CustomListTile({
-    required this.item,
+    required this.menuItem,
   });
 
-  final MenuItem item;
+  final MenuItem menuItem;
 
   @override
   Widget build(BuildContext context) {
@@ -49,12 +50,17 @@ class _CustomListTile extends StatelessWidget {
     final colors = Theme.of(context).colorScheme;
 
     return ListTile(
-      title: Text(item.title),
-      subtitle: Text(item.subTitle),
-      leading: Icon(item.icon, color: colors.primary,),
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subTitle),
+      leading: Icon(menuItem.icon, color: colors.primary,),
       trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary,),
       onTap: () {
-        //TODO: Navigate to anothers screens
+        // Navigator.of(context).push(
+        //   MaterialPageRoute(
+        //     builder: (context) => const ButtonsScreen(),
+        //   )
+        // );
+        Navigator.pushNamed(context, menuItem.link);
       },
     );
   }
