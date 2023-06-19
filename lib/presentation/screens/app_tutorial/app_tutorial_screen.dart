80734831e8b573a75db62eb2fc7a985398de6efa
Jonathan Martinez <jonathanmartzca@gmail.com>
2023-06-19 11:23:53 -0600

    screens and barrel updated

---
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppTutoralScreen extends StatelessWidget {
+
+  static const name = 'tutorial_screen';
+
+  const AppTutoralScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
