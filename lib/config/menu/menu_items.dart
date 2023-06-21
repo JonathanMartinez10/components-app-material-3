ca769cda37b6c4e3aa5f9cd916faba63571da32c
Jonathan Martinez <jonathanmartzca@gmail.com>
2023-06-20 22:03:41 -0600

    ui screen finished

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -50,4 +50,11 @@ const appMenuItems = <MenuItem>[
     link: '/animated', 
     icon: Icons.check_box_outline_blank_rounded,
   ),
+
+  MenuItem(
+    title: 'UI Controls + Tiles', 
+    subTitle: 'Controles en Flutter', 
+    link: '/ui-controls', 
+    icon: Icons.car_rental_outlined,
+  ),
 ];
